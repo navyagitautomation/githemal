7abc9d45a41918cd06968606154f999653b40c27
Navya <kbnavya@gmail.com>
2022-05-04 18:08:27 +0530

    first commit

---
+++ cucumber1/src/test/java/Runner/testrunner.java
@@ -0,0 +1,15 @@
+package Runner;
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src\\test\\java\\feature\\login.feature",glue={"stepdefnition"},monochrome= true,
+//plugin= {"pretty","html:target/htmlreports/HtmlReports"}
+//plugin= {"pretty","json:target/jsonReports/report.json"}
+plugin= {"pretty","junit:target/jUnit1reports/report.xml"}	
+		)
+public class testrunner {
+
+}
---
+++ cucumber1/src/test/java/stepdefnition/steodefnition.java
@@ -0,0 +1,38 @@
+package stepdefnition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class steodefnition {
+	
+	@Given("user is  on homepage")
+	public void user_is_on_homepage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    
+	    System.out.println("Given");
+	}
+
+	@When("user navigates to Login Page")
+	public void user_navigates_to_login_page() {
+	    // Write code here that turns the phrase above into concrete actions
+		 System.out.println("When");
+
+	}
+
+	@And("user enters username and Password")
+	public void user_enters_username_and_password() {
+	    // Write code here that turns the phrase above into concrete actions
+		 System.out.println("and");
+
+	}
+
+	@Then("success message is displayed")
+	public void success_message_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+		 System.out.println("then");
+
+	}
+
+}
